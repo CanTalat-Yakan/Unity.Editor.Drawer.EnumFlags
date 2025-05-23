dc0c677b1c977b3c5dc6d99975bd0d55886af1bf
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-23 11:44:01 +0200

    ⠀⠀⠀⠀⠀

--- Editor/EnumFlagsDrawer.cs
+++ Editor/EnumFlagsDrawer.cs
@@ -5,6 +5,12 @@ using UnityEditor;
 
 namespace UnityEssentials
 {
+    /// <summary>
+    /// A custom property drawer for rendering enums with the <see cref="EnumFlagsAttribute"/> in the Unity Editor.
+    /// </summary>
+    /// <remarks>This drawer provides a foldout interface for selecting multiple enum values using checkboxes.
+    /// It also includes "All" and "None" buttons for quickly selecting or deselecting all options. The drawer is
+    /// designed to work with enums that have the <see cref="FlagsAttribute"/> applied.</remarks>
     [CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
     public class EnumFlagsDrawer : PropertyDrawer
     {
@@ -14,9 +20,25 @@ namespace UnityEssentials
         private Array _enumValues;
         private Type _enumUnderlyingType;
 
+        /// <summary>
+        /// Calculates the height of the property field in the editor, accounting for foldout state and the number of
+        /// enum values.
+        /// </summary>
+        /// <param name="property">The serialized property being drawn in the editor.</param>
+        /// <param name="label">The label to display alongside the property field.</param>
+        /// <returns>The height, in pixels, required to render the property field.</returns>
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label) =>
             EditorGUIUtility.singleLineHeight * (_foldoutOpen ? Enum.GetValues(fieldInfo.FieldType).Length + 2 : 1);
 
+        /// <summary>
+        /// Renders a custom GUI for an enum property with support for flag-based selection.
+        /// </summary>
+        /// <remarks>This method provides a custom inspector interface for enum fields marked with a
+        /// specific attribute, allowing users to toggle individual flags or select "All" or "None" options. If the
+        /// property is not an enum, an error message is displayed instead.</remarks>
+        /// <param name="position">The rectangle on the screen to use for the property GUI.</param>
+        /// <param name="property">The serialized property being drawn. Must be of type <see cref="SerializedPropertyType.Enum"/>.</param>
+        /// <param name="label">The label to display alongside the property field.</param>
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             if (property.propertyType != SerializedPropertyType.Enum)
@@ -36,7 +58,8 @@ namespace UnityEssentials
 
             EditorGUI.BeginProperty(position, label, property);
 
-            _foldoutOpen = EditorGUI.Foldout(new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight), _foldoutOpen, label);
+            var foldoutPosition = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+            _foldoutOpen = EditorGUI.Foldout(foldoutPosition, _foldoutOpen, label, true);
 
             if (_foldoutOpen)
             {
@@ -63,9 +86,27 @@ namespace UnityEssentials
             property.serializedObject.ApplyModifiedProperties();
         }
 
+        /// <summary>
+        /// Retrieves the value of an enumeration at the specified index.
+        /// </summary>
+        /// <remarks>This method assumes that the enumeration values are stored in a collection and that
+        /// the underlying type of the enumeration is known. The returned value is converted to the enumeration's
+        /// underlying type.</remarks>
+        /// <param name="index">The zero-based index of the enumeration value to retrieve.</param>
+        /// <returns>The enumeration value at the specified index, converted to its underlying type.</returns>
         private object GetEnumValue(int index) =>
             Convert.ChangeType(_enumValues.GetValue(index), _enumUnderlyingType);
 
+        /// <summary>
+        /// Toggles the inclusion of a specific enum value in the current state.
+        /// </summary>
+        /// <remarks>If <paramref name="set"/> is <see langword="true"/> and the specified index
+        /// corresponds to a "None" element, the current state is reset to the default value. If <paramref name="set"/>
+        /// is <see langword="false"/> and the specified index corresponds to a "None" or "All" element, the operation
+        /// is ignored.</remarks>
+        /// <param name="index">The index of the enum value to toggle. Must correspond to a valid enum value.</param>
+        /// <param name="set">A boolean value indicating whether to include or exclude the enum value. <see langword="true"/> to include
+        /// the value; <see langword="false"/> to exclude it.</param>
         private void ToggleIndex(int index, bool set)
         {
             if (set)
@@ -90,6 +131,16 @@ namespace UnityEssentials
 
         }
 
+        /// <summary>
+        /// Determines whether the specified enum element, identified by its index, is set.
+        /// </summary>
+        /// <remarks>This method handles special cases for "All" and "None" elements in the enum: <list
+        /// type="bullet"> <item> For "All" elements, it checks whether all other visible elements are set, accounting
+        /// for any missing bits. </item> <item> For "None" elements, it checks whether the enum's value is zero.
+        /// </item> </list> For all other elements, the method performs a bitwise operation to determine if the element
+        /// is set.</remarks>
+        /// <param name="index">The zero-based index of the enum element to check.</param>
+        /// <returns><see langword="true"/> if the specified enum element is set; otherwise, <see langword="false"/>.</returns>
         private bool IsSet(int index)
         {
             object value = DoAndOperator(_theEnum, GetEnumValue(index), _enumUnderlyingType);
@@ -123,6 +174,15 @@ namespace UnityEssentials
             return !value.Equals(Convert.ChangeType(0, _enumUnderlyingType));
         }
 
+        /// <summary>
+        /// Performs a bitwise OR operation on two objects of the specified type.
+        /// </summary>
+        /// <param name="a">The first operand. Must be of the specified <paramref name="type"/>.</param>
+        /// <param name="b">The second operand. Must be of the specified <paramref name="type"/>.</param>
+        /// <param name="type">The type of the operands. Must be a supported integral type (e.g., <see cref="int"/>, <see cref="uint"/>,
+        /// <see cref="short"/>, etc.).</param>
+        /// <returns>The result of the bitwise OR operation, cast to the specified <paramref name="type"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="type"/> is not a supported integral type.</exception>
         private static object DoOrOperator(object a, object b, Type type)
         {
             return type switch
@@ -139,6 +199,16 @@ namespace UnityEssentials
             };
         }
 
+        /// <summary>
+        /// Performs a bitwise AND operation on two objects of the specified type.
+        /// </summary>
+        /// <param name="a">The first operand. Must be an object of the specified <paramref name="type"/>.</param>
+        /// <param name="b">The second operand. Must be an object of the specified <paramref name="type"/>.</param>
+        /// <param name="type">The type of the operands. Supported types include <see cref="int"/>, <see cref="uint"/>, <see
+        /// cref="short"/>, <see cref="ushort"/>, <see cref="long"/>, <see cref="ulong"/>, <see cref="byte"/>, and <see
+        /// cref="sbyte"/>.</param>
+        /// <returns>The result of the bitwise AND operation, cast to an object of the specified <paramref name="type"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="type"/> is not one of the supported types.</exception>
         private static object DoAndOperator(object a, object b, Type type)
         {
             return type switch
@@ -155,6 +225,18 @@ namespace UnityEssentials
             };
         }
 
+        /// <summary>
+        /// Performs a bitwise NOT operation on the specified value, based on its type.
+        /// </summary>
+        /// <remarks>Supported types for the <paramref name="type"/> parameter include: <see cref="int"/>,
+        /// <see cref="uint"/>, <see cref="short"/>, <see cref="ushort"/>, <see cref="long"/>, <see cref="ulong"/>, <see
+        /// cref="byte"/>, and <see cref="sbyte"/>.</remarks>
+        /// <param name="a">The value to apply the bitwise NOT operation to. The type of this value must match the specified <paramref
+        /// name="type"/>.</param>
+        /// <param name="type">The type of the value <paramref name="a"/>. Must be a supported integral type (e.g., <see cref="int"/>, <see
+        /// cref="uint"/>, <see cref="short"/>, etc.).</param>
+        /// <returns>The result of the bitwise NOT operation, cast to the specified type.</returns>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="type"/> is not a supported integral type.</exception>
         private static object DoNotOperator(object a, Type type)
         {
             return type switch
--- Runtime/EnumFlagsAttribute.cs
+++ Runtime/EnumFlagsAttribute.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 
 namespace UnityEssentials
 {
+    /// <summary>
+    /// Indicates that an enumeration field should be treated as a set of bitwise flags.
+    /// </summary>
+    /// <remarks>This attribute is typically applied to enumeration fields to signify that the values  can be
+    /// combined using bitwise operations. It is commonly used in conjunction with  enums marked with the <see
+    /// cref="FlagsAttribute"/>.</remarks>
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
     public class EnumFlagsAttribute : PropertyAttribute { }
 }
